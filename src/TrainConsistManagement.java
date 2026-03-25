cc4bc529752a6242b138b450fa4224ddc5843415
JayalakshmiJL25 <keerthijaya7777@gmail.com>
2026-03-25 09:09:02 +0530

    UC1: Added main class, initialized dynamic List (ArrayList), and displayed initial consist summary

--- src/TrainConsistManagement.java
+++ src/TrainConsistManagement.java
@@ -1,2 +1,20 @@
-public class TrainConsistManagement{
+import java.util.ArrayList;
+import java.util.List;
+
+public class TrainConsistManagement {
+
+    public static void main(String[] args) {
+
+        // Welcome Message
+        System.out.println("=== Train Consist Management App ===");
+
+        // Initialize Train Consist (Dynamic Collection)
+        List<String> trainConsist = new ArrayList<>();
+
+        // Display Initial Bogie Count
+        System.out.println("Train consist initialized successfully.");
+        System.out.println("Initial number of bogies: " + trainConsist.size());
+
+        System.out.println("Program is ready to manage the train consist...");
+    }
 }
